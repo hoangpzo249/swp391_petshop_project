191253c994c008d6978b70c89466ed2f359d32e8
hoangpzo <filmmusic249@gmail.com>
2025-05-29 08:17:24 +0700

    homepage v1fix

---
+++ src/java/DAO/DBContext.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author HuyHoang
+ */
+public class DBContext {
+
+    private final String serverName = "localhost";
+    private final String dbName = "PetShopFPT_DB";
+    private final String portNumber = "1433";
+    private final String userID = "sa";
+    private final String password = "123";
+
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=true;";
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+
+    public void closeConnection(Connection con, PreparedStatement ps, ResultSet rs) throws Exception {
+        if (rs != null && !rs.isClosed()) {
+            rs.close();
+        }
+        if (ps != null && !ps.isClosed()) {
+            ps.close();
+        }
+        if (con != null && !con.isClosed()) {
+            con.close();
+        }
+    }
+}
