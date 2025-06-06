eca5ebe03ec2427df989c19b89dde83a7456102e
NamNNG01 <ngnam0178@gmail.com>
2025-06-06 09:12:33 +0700

    displaycartServlet+test

---
+++ src/java/Controllers/DisplayCartServlet.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controllers;
+
+import DAO.CartDAO;
+import DAO.PetDAO;
+import Models.Account;
+import Models.Cart;
+import Models.Pet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayCartServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DisplayCart</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet DisplayCart at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        CartDAO cartDao = new CartDAO();
+        PetDAO petDao = new PetDAO();
+        HttpSession session = request.getSession();
+
+        List<Cart> petCart = new ArrayList<>();
+        List<Pet> pets = new ArrayList<>();
+        double totalPrice = 0;
+
+        Account account = (Account) session.getAttribute("account");
+        if (account != null) {
+            int accId = account.getAccId();
+            List<Cart> carts = cartDao.getCart(accId);
+            if (carts != null && !carts.isEmpty()) {
+                petCart = carts;
+            } else {
+                request.setAttribute("cartMessage", "Giỏ hàng của bạn đang trống.");
+            }
+            request.setAttribute("name", account.getAccFname() + " " + account.getAccLname());
+            request.setAttribute("phone", account.getAccPhoneNumber());
+            request.setAttribute("address", account.getAccAddress());
+
+        } else {
+            Object guestCart = session.getAttribute("guestCart");
+            if (guestCart != null) {
+                petCart = (List<Cart>) guestCart;
+            }
+        }
+
+        List<Cart> updatedPetCart = new ArrayList<>();
+
+        for (Cart c : petCart) {
+            if (c.getPetId() != null) {
+                Pet pet = petDao.getPetById(c.getPetId());
+                if (pet != null && pet.getPetAvailability() == 1) {
+                    pets.add(pet);
+                    double price = pet.getPetPrice();
+                    c.setPrice(price);
+                    totalPrice += price;
+                    updatedPetCart.add(c);
+                } else {
+                    if (account != null) {
+                        cartDao.deleteFromPetCart(account.getAccId(), c.getPetId());
+                    }
+                }
+            }
+        }
+
+
+        if (account != null) {
+            session.setAttribute("cartcount", cartDao.getTotalCartItems(account.getAccId()));
+        } else {
+            session.setAttribute("guestCart", updatedPetCart);
+            session.setAttribute("cartcount", updatedPetCart.size());
+        }
+
+        request.setAttribute("petList", updatedPetCart);
+        request.setAttribute("pets", pets);
+        request.setAttribute("totalPrice", totalPrice);
+
+        request.getRequestDispatcher("cart.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
